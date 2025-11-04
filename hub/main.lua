5128306f6d84ba37e7fd26cd70b37dfc99b52886
Alex <makargravanov@gmail.com>
2025-11-04 18:00:47 +0300

    fix

--- hub/main.lua
+++ hub/main.lua
@@ -5,10 +5,16 @@ function Main.run()
     local hubState = require("hub/hub_state").new()
     local droneService = require("hub/services/drone_service").new(hubState)
     local hub = require("hub.hub").new(hubState, droneService)
-    hub:initialize()
     
-    hub:listenCommands()
-    
+    -- Run initialize in a separate thread, main thread handles listening
+    parallel.waitForAll(
+        function()
+            hub:initialize()
+        end,
+        function()
+            hub:listenCommands()
+        end
+    )
 end
 
 return Main
