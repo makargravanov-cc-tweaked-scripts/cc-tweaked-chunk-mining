b3a328c60f3ea927b7c8aa8241c905b5306dd8ca
Alex <makargravanov@gmail.com>
2025-11-13 10:01:30 +0300

    fix

--- run_drone.lua
+++ run_drone.lua
@@ -1,7 +1,10 @@
 
-rednet.open("right")
-if rednet.isOpen("right") == false then
-    print("Turn on modem please!")
+rednet.open("left")
+if rednet.isOpen("left") == false then
+    rednet.open("right")
+    if rednet.isOpen("right") == false then
+        print("Turn on modem please!")
+    end
 end
 
 local Main = require("drone.main")
